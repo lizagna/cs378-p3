ce5d7f1bf5738f1bfe61ade518f6ba84c6ad23df
lizagna <lizagna@Elizabeths-MacBook-Pro.local>
2023-02-28 16:44:10 -0600

    general layout

--- cs378-p3/src/App.js
+++ cs378-p3/src/App.js
@@ -1,37 +1,63 @@
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [city, setCity] = useState({
+    "Austin":{"latitude":30.26715,"longitude":-97.74306},
+    "Dallas":{"latitude":32.78306,"longitude":-96.80667},
+    "Houston":{"latitude":29.76328,"longitude":-95.36327}
+  });
+
+
+  
+  
   return (
     <body>
-      <div className="App">
-        <table className="Menu">
-          <tr className="Button-row">
-            <td>
-              <button>Austin</button>
-            </td>
-            <td>
-              <button>Dallas</button>
-            </td>
-            <td>
-            <button>Houston</button>
-            </td>
-          </tr>
+      <table className="Menu">
+        <tr className="Button-row">
+          <td>
+            <button>
+              <b>Austin</b>
+            </button>
+          </td>
+          <td>
+            <button>
+              <b>Dallas</b>
+            </button>
+          </td>
+          <td>
+            <button>
+              <b>Houston</b>
+            </button>
+          </td>
+        </tr>
 
-          <tr className="Search-bar">
-            <td>
-              <form>
-                <label>
-                  <input type="text" />
-                </label>
-              </form>
-            </td>
-            <td>
-              <button>+</button>
-            </td>
-          </tr>
-        </table>
-      </div>
+        <tr className="Search-bar">
+          <td>
+            <form>
+              <label>
+                <input type="text" />
+              </label>
+            </form>
+          </td>
+          <td>
+            <button>
+              <b>+</b>
+            </button>
+          </td>
+        </tr>
+      </table>
+
+      <table className="Weather-data">
+        <tr className="Weather-data-header">
+          <td> 
+            <b>Time</b> 
+          </td>
+          <td>
+            <b>Temperature</b>
+          </td>
+        </tr>
+      </table>
     </body>
     
   );
